63887068ad3458ac9b4761d7d4a0469ddd6bf6fd
ranhaogong <ranhaogong@qq.com>
2025-03-12 14:25:13 +0800

    MRGD

--- script/test_building3d_save.sh
+++ script/test_building3d_save.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICES=3 \
+CUDA_VISIBLE_DEVICES=2 \
 python ../test_save_building3d.py \
 --data_path /data/haoran/dataset/building3d/Point2Roof \
 --cfg_file ../cfg/model_cfg_color_fpfh_lovasz_2048.yaml \
---test_tag building3d_all_ptv3_color_2048_adamw_cosine_lr4_epoch150_fpfh_lovasz_wavelet_graph \
+--test_tag building3d_all_ptv3_color_2048_adamw_cosine_lr4_epoch150_fpfh_lovasz_enhanceptv3 \
 --batch_size 64
